130b6081eed4aabaf93827cfd4ea461af06c0dde
May Turner <amturnr@gmail.com>
2022-05-12 08:21:57 -0600

    TNUM

--- Tnum_Ingest.R
+++ Tnum_Ingest.R
@@ -1,6 +1,7 @@
 ##Ingest with the new function - test data (acs5_race) can be pulled with the PullingData R script
 library(tnum)
 library(jsonlite)
+library(tidyverse)
 
 creds <- "alison@metricstogether.com:brushfire"
 ip <- "metrics.truenum.com:8080"
@@ -10,16 +11,19 @@ tnum.setSpace("MetricsTogether")
 #this is the original way I pulled the data from Pulling Data. I created a csv of this information for ease of sharing
 # set.seed(123)
 # race_sample <- acs5_race[sample(1:723287,10),]
-race_sample <- read.csv(race_sample)
+race_sample <- read_csv("race_sample.csv")
 
-templates <- list(
-  #questions for allen
-    #1. previously we made the state the subject path, how can we do that now?
-    #2. How do we put in tags?
-    #3. Is there a way to use the column headers as the name? ie instead of writing "white alone population" pull that from the column name?
-  
-  c("$(county) $(tract) has white alone population = $`white alone`")
-)
+
+templates <- c()
+
+for (i in 5:length(race_sample)) {
+  templates[[(length(templates) + 1)]] <- c(paste0("tract $(tract) in $(county) has ",names(race_sample)[i],  " population = $(",names(race_sample)[i],")"),"data:USFederal:ACS5:$(year)")
+  i <- i+1
+}
+
+template <- list(c("tract $(tract) in $(county) has white alone population = $(white alone)", "data:USFederal:ACS5:$(year)"))
 
 #this is the desired result Appling_County:tract:950100 has Over_65_male_population = 258 -- tag==data:USFederal:ACS5:YEAR*
-tnum.ingestDataFrame(head(race_sample),templates,"testIngest1.txt")
+tnum.ingestDataFrame(head(race_sample),template,"testIngest1.txt")
+
+
